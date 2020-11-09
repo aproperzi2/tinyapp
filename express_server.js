bb0eee0cef5941923040d9923514dda89be42b36
Luca Properzi <abramoproperzi@gmail.com>
2020-11-09 22:13:54 +0000

    added view for single URL

--- express_server.js
+++ express_server.js
@@ -24,7 +24,12 @@ app.get('/hello', (req, res) => {
 app.get('/urls', (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
-})
+});
+
+app.get('/urls/:shortURL', (req, res) => {
+  const templateVars = { shortURL:req.params.shortURL, longURL: req.params.longURL };
+  res.render('urls_show', templateVars);
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
