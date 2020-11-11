e39ede46c2083fef473ae37079c37dce5c1e1029
Luca Properzi <abramoproperzi@gmail.com>
2020-11-11 01:09:54 +0000

    delete and update url features implemented

--- express_server.js
+++ express_server.js
@@ -17,21 +17,15 @@ function generateRandomString() {
   return result;
 }
 
+// database
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-app.get('/', (req, res) => {
-  res.send('Hello!');
-});
-
+// Urls Database in json format
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
-});
-
-app.get('/hello', (req, res) => {
-  res.send('<html><body>Hello <b>World</b></body></html>\n');
 });
 
 app.get('/urls', (req, res) => {
@@ -50,16 +44,30 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${shortURL}`);  
 });
 
+app.post('/urls/:shortURL/delete', (req, res) => {
+  const shortURL = req.params.shortURL;
+  delete urlDatabase[shortURL];
+  res.redirect('/urls');
+});
+
+app.post('/urls/:shortURL/update', (req, res) => {
+  const longURL = req.params.longURL;
+  urlDatabase[req.params.shortURL] = req.body.longURL;
+  res.redirect('/urls');
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
 
+// Get single url after a new url submission
 app.get('/u/:shortURL', (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
 
+// Listening on PORT 8080
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
